c8a2997affd6c95d6996ceed96d99e5af322e3c1
AlokGDAAS <ankit25792kumar@gmail.com>
2025-06-13 18:00:24 +0530

    june 13 class task 1 completed

---
+++ june13.py
@@ -0,0 +1,19 @@
+def sum(a,b):
+    return a+b
+
+def subtraction(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def devide(a,b):
+    return a/b
+
+num1 =int(input("Enter your first number: "))
+num2 =int(input("Enter your second number: "))
+
+print("Sum : ",sum(num1,num2))
+print("Subraction : ",subtraction(num1,num2))
+print("Multiply : ",multiply(num1,num2))
+print("Devide : ",devide(num1,num2))
